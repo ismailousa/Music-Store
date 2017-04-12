7a567a562809f61f14228636622932cc47a6615d
ISMAILOUASUS\isma_ <ismailou.sa@gmail.com>
2017-04-13 01:48:08 +0300

    HomeController and StoreController added

---
+++ Music Store/Controllers/StoreController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Music_Store.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            return Content("Hello from Store.Index()");
+        }
+        //public ActionResult Browse()
+        //{
+        //    return Content("Hello from Store.Browse()");
+        //}
+        public ActionResult Browse(string genre)
+        {
+            string message = HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
+            return Content(message);
+        }
+        public ActionResult Details(int id = 0)
+        {
+            return Content("Store.Details, ID = " + id);
+        }
+    }
+}
