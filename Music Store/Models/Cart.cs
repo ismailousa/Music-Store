98fcff6aa02b5b084ad081a89a4693dcc18935a1
ISMAILOUASUS\isma_ <ismailou.sa@gmail.com>
2017-04-20 00:50:12 +0300

    Shopping Cart business logic added

--- Music Store/Models/Cart.cs
+++ Music Store/Models/Cart.cs
@@ -10,7 +10,7 @@ namespace Music_Store.Models
     {
         [Key]
         public int RecordId { get; set; }
-        public string CardId { get; set; }
+        public string CartId { get; set; }
         public int AlbumId { get; set; }
         public int Count { get; set; }
         public System.DateTime DateCreated { get; set; }
